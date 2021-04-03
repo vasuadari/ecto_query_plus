8928f64d2cc90050394339283c5a5f78e7290d31
Vasu Adari <vasu.adari@go-jek.com>
2021-04-03 15:13:42 +0530

    Update mix.exs to publish 0.1.0 version

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule EctoQueryPlus.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/vasuadari/constant"
+  @version "0.1.0"
+
   def project do
     [
       app: :ecto_query_plus,
-      version: "0.1.0",
+      description: "Lets you compose ecto queries in a functional way.",
+      docs: docs(),
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -20,6 +26,27 @@ defmodule EctoQueryPlus.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [ecto: "~> 3.0"]
+    [
+      {:ecto, "~> 3.0"},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "ecto_query_plus",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/vasuadari/ecto_query_plus",
+      homepage_url: "https://hexdocs.pm/ecto_query_plus/readme.html#content",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      name: "ecto_query_plus",
+      licenses: ["MIT"],
+      links: %{"Github" => @github_url}
+    ]
   end
 end

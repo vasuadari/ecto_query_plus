7892ba78f5244ac6eff9bc97605cdc8e49c1d4f5
Vasu Adari <vasu.adari@go-jek.com>
2021-04-04 20:19:07 +0530

    Upgrade version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule EctoQueryPlus.MixProject do
   use Mix.Project
 
   @github_url "https://github.com/vasuadari/ecto_query_plus"
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [

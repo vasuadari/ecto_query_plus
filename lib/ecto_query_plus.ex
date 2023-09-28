27d15b6ed7c5fd8390c06432cc1a22557f098c22
Vasu Adari <vasu.adari@scripbox.com>
2023-09-28 11:00:24 +0530

    update ecto_query_plus to support both import & use

--- lib/ecto_query_plus.ex
+++ lib/ecto_query_plus.ex
@@ -1,8 +1,89 @@
 defmodule EctoQueryPlus do
+  import EctoQueryPlus.BaseQuery
+
+  def wherep(query, nil), do: query
+  def wherep(query, []), do: query
+
+  def wherep(query, [{field, :gte, value} | other_filters]) do
+    query
+    |> ecto_where([t], field(t, ^field) >= ^value)
+    |> wherep(other_filters)
+  end
+
+  def wherep(query, [{field, :gt, value} | other_filters]) do
+    query
+    |> ecto_where([t], field(t, ^field) > ^value)
+    |> wherep(other_filters)
+  end
+
+  def wherep(query, [{field, :lt, value} | other_filters]) do
+    query
+    |> ecto_where([t], field(t, ^field) < ^value)
+    |> wherep(other_filters)
+  end
+
+  def wherep(query, [{field, :lte, value} | other_filters]) do
+    query
+    |> ecto_where([t], field(t, ^field) <= ^value)
+    |> wherep(other_filters)
+  end
+
+  def wherep(query, [{field, :ne, value} | other_filters]) do
+    query
+    |> ecto_where([t], field(t, ^field) != ^value)
+    |> wherep(other_filters)
+  end
+
+  def wherep(query, [{field, :ilike, value} | other_conds]) do
+    query
+    |> ecto_where([t], ilike(field(t, ^field), ^"%#{value}%"))
+    |> wherep(other_conds)
+  end
+
+  def wherep(query, [{field, nil} | other_filters]) do
+    query
+    |> ecto_where([t], is_nil(field(t, ^field)))
+    |> wherep(other_filters)
+  end
+
+  def wherep(query, [{field, :not_nil} | other_filters]) do
+    query
+    |> ecto_where([t], not is_nil(field(t, ^field)))
+    |> wherep(other_filters)
+  end
+
+  def wherep(query, [{field, value} | other_filters]) when is_list(value) do
+    query
+    |> ecto_where([t], field(t, ^field) in ^value)
+    |> wherep(other_filters)
+  end
+
+  def wherep(query, [{field, value} | other_filters]) do
+    query
+    |> ecto_where([t], field(t, ^field) == ^value)
+    |> wherep(other_filters)
+  end
+
+  def offsetp(query, nil), do: query
+  def offsetp(query, offset), do: query |> ecto_offset(^offset)
+
+  def limitp(query, nil), do: query
+  def limitp(query, limit), do: query |> ecto_limit(^limit)
+
+  def selectp(query, nil), do: query
+  def selectp(query, [] = _only), do: query |> ecto_select([c], c)
+  def selectp(query, only), do: query |> ecto_select(^only)
+
+  def order_byp(query, nil), do: query
+  def order_byp(query, {field, :asc}), do: query |> ecto_order_by(asc: ^field)
+  def order_byp(query, {field, :desc}), do: query |> ecto_order_by(desc: ^field)
+
+  def preloadp(query, nil), do: query
+  def preloadp(query, [] = _associations), do: query
+  def preloadp(query, associations), do: query |> ecto_preload(^associations)
+
   defmacro __using__(_opts) do
     quote do
-      import EctoQueryPlus.BaseQuery
-
       def list(struct, filters, options \\ %{}) do
         struct
         |> wherep(filters)
@@ -13,86 +94,12 @@ defmodule EctoQueryPlus do
         |> preloadp(options[:preload])
       end
 
-      def wherep(query, nil), do: query
-      def wherep(query, []), do: query
-
-      def wherep(query, [{field, :gte, value} | other_filters]) do
-        query
-        |> ecto_where([t], field(t, ^field) >= ^value)
-        |> wherep(other_filters)
-      end
-
-      def wherep(query, [{field, :gt, value} | other_filters]) do
-        query
-        |> ecto_where([t], field(t, ^field) > ^value)
-        |> wherep(other_filters)
-      end
-
-      def wherep(query, [{field, :lt, value} | other_filters]) do
-        query
-        |> ecto_where([t], field(t, ^field) < ^value)
-        |> wherep(other_filters)
-      end
-
-      def wherep(query, [{field, :lte, value} | other_filters]) do
-        query
-        |> ecto_where([t], field(t, ^field) <= ^value)
-        |> wherep(other_filters)
-      end
-
-      def wherep(query, [{field, :ne, value} | other_filters]) do
-        query
-        |> ecto_where([t], field(t, ^field) != ^value)
-        |> wherep(other_filters)
-      end
-
-      def wherep(query, [{field, :ilike, value} | other_conds]) do
-        query
-        |> ecto_where([t], ilike(field(t, ^field), ^"%#{value}%"))
-        |> wherep(other_conds)
-      end
-
-      def wherep(query, [{field, nil} | other_filters]) do
-        query
-        |> ecto_where([t], is_nil(field(t, ^field)))
-        |> wherep(other_filters)
-      end
-
-      def wherep(query, [{field, :not_nil} | other_filters]) do
-        query
-        |> ecto_where([t], not is_nil(field(t, ^field)))
-        |> wherep(other_filters)
-      end
-
-      def wherep(query, [{field, value} | other_filters]) when is_list(value) do
-        query
-        |> ecto_where([t], field(t, ^field) in ^value)
-        |> wherep(other_filters)
-      end
-
-      def wherep(query, [{field, value} | other_filters]) do
-        query
-        |> ecto_where([t], field(t, ^field) == ^value)
-        |> wherep(other_filters)
-      end
-
-      def offsetp(query, nil), do: query
-      def offsetp(query, offset), do: query |> ecto_offset(^offset)
-
-      def limitp(query, nil), do: query
-      def limitp(query, limit), do: query |> ecto_limit(^limit)
-
-      def selectp(query, nil), do: query
-      def selectp(query, [] = _only), do: query |> ecto_select([c], c)
-      def selectp(query, only), do: query |> ecto_select(^only)
-
-      def order_byp(query, nil), do: query
-      def order_byp(query, {field, :asc}), do: query |> ecto_order_by(asc: ^field)
-      def order_byp(query, {field, :desc}), do: query |> ecto_order_by(desc: ^field)
-
-      def preloadp(query, nil), do: query
-      def preloadp(query, [] = _associations), do: query
-      def preloadp(query, associations), do: query |> ecto_preload(^associations)
+      defdelegate wherep(struct, filters), to: EctoQueryPlus
+      defdelegate offsetp(struct, filters), to: EctoQueryPlus
+      defdelegate limitp(struct, filters), to: EctoQueryPlus
+      defdelegate selectp(struct, filters), to: EctoQueryPlus
+      defdelegate order_byp(struct, filters), to: EctoQueryPlus
+      defdelegate preloadp(struct, filters), to: EctoQueryPlus
     end
   end
 end

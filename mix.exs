16191e422ef8e050dba885c5b7152eca3be53bf8
Vasu Adari <vasu.adari@scripbox.com>
2023-12-19 09:12:22 +0530

    fix ecto versioning condition

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule EctoQueryPlus.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, ">= 3.0"},
+      {:ecto, ">= 3.0.0"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
